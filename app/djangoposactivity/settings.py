90a5fa09c70270202193302db96efcd982eb82ef
Ian Red Ligot <redligot2009.forreal@gmail.com>
2021-05-27 06:20:47 +0800

    Trigger a rebuild / redeploy to see why collectstatic doesn't appear to work...

--- app/djangoposactivity/settings.py
+++ app/djangoposactivity/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/staticfiles/"
-
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+MEDIA_URL = '/mediafiles/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
--- app/entrypoint.prod.sh
+++ app/entrypoint.prod.sh
@@ -11,7 +11,7 @@ then
     echo "PostgreSQL started"
 fi
 
-python manage.py migrate
 python manage.py collectstatic --no-input --clear
+python manage.py migrate
 
 exec "$@"
