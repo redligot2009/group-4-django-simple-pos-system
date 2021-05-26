f3dce6c5416650d0b502f570ee77682245f08c37
Ian Red Ligot <redligot2009.forreal@gmail.com>
2021-05-27 05:56:56 +0800

    Added public IP address in allowed hosts

--- app/djangoposactivity/settings.py
+++ app/djangoposactivity/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY','4cw-%zlf-x)g@95n5v&fjkeslt--evj3s*!q+!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'ec2-35-173-164-153.compute-1.amazonaws.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '35.173.164.153', 'https://ec2-35-173-164-153.compute-1.amazonaws.com/']
 
 # # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = os.environ.get("SECRET_KEY")
