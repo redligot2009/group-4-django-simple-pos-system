73c10b7cc3c45b2cce3cbc021afc10e631014b9e
Ian Red Ligot <redligot2009.forreal@gmail.com>
2021-05-28 05:15:09 +0800

    Updated deploy.sh to start docker service every deploy if it isn't started yet

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 ssh -o StrictHostKeyChecking=no ec2-user@$EC2_PUBLIC_IP_ADDRESS << 'ENDSSH'
   cd /home/ec2-user/app
   export $(cat .env | xargs)
+  sudo service docker start
   docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
   docker pull $IMAGE:web
   docker pull $IMAGE:nginx
