7a1cad9b008c285c2d535d19c9d57427cc3485f7
Ian Red Ligot <redligot2009.forreal@gmail.com>
2021-06-02 13:33:37 +0800

    Attempt to fix env secret key error.

--- app/djangoposactivity/settings.py
+++ app/djangoposactivity/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY','4cw-%zlf-x)g@95n5v&fjkeslt--evj3s*!q+!6atil_-rf$c=')
+SECRET_KEY = os.environ.get('SECRET_KEY',default='4cw-%zlf-x)g@95n5v&fjkeslt--evj3s*!q+!6atil_-rf$c=')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
