6a6c20ffecbd8581a053bb66f0253c73aa9c57d8
Ian Red Ligot <redligot2009.forreal@gmail.com>
2021-06-02 12:12:22 +0800

    Updated tests

--- app/posapp/tests.py
+++ app/posapp/tests.py
@@ -1,8 +1,12 @@
-from django.test import TestCase
+from logging import log
+from django.test import TestCase, Client
 
 from .models import Order, Item, OrderItem
 
 from django.core import exceptions
+from django.http.response import Http404, HttpResponseBadRequest, HttpResponseNotAllowed
+from django.urls import reverse
+
 
 """
 Model tests for:
@@ -78,3 +82,48 @@ class ItemTests(TestCase):
         sample_item = sample_items.filter(id=sample_item.id).delete()
         sample_items = Item.objects.all()
         self.assertEqual(len(sample_items),0)
+
+"""
+Tests for views:
+- create_order
+- order_details
+- list_orders
+- update_order
+"""
+
+class ItemViewTests(TestCase):
+
+    def test_create_item(self):
+        items = Item.objects.all()
+        cnt_items = len(items)
+        response = self.client.post('/add_item',
+            {"item_name": "Test Item",
+            "item_price": 50})
+        self.assertEqual(response.status_code, 302)
+        items = Item.objects.all()
+        self.assertTrue(len(items) > cnt_items)
+
+    def test_update_item(self):
+        self.test_create_item()
+        item_to_update = Item.objects.first()
+        item_pk = item_to_update.id
+        response = self.client.post(reverse('update_item',kwargs={'pk':item_pk}),
+            {"item_name": "Test Item A",
+            "item_price": 25,
+            "stock_quantity": 15})
+        self.assertEqual(response.status_code, 302)
+        item_to_update = Item.objects.get(id=item_pk)
+        self.assertEqual(item_to_update.item_name,"Test Item A")
+        self.assertEqual(item_to_update.stock_quantity,15)
+        self.assertEqual(item_to_update.item_price,25)
+
+class OrderViewTests(TestCase):
+
+    def test_create_order(self):
+        orders = Order.objects.all()
+        order_cnt = len(orders)
+        response = self.client.post("/orders/create")
+        self.assertEqual(response.status_code, 302)
+        orders = Order.objects.all()
+        self.assertTrue(len(orders) > order_cnt)
+
--- app/posapp/views.py
+++ app/posapp/views.py
@@ -1,86 +1,119 @@
-from django.http.response import Http404, HttpResponseBadRequest
+from django.http.response import Http404, HttpResponseBadRequest, HttpResponseNotAllowed, HttpResponseNotFound, HttpResponseServerError
 from django.shortcuts import get_object_or_404, render, redirect
 from .models import Item, Order, OrderItem
 import logging
 logger = logging.getLogger(__name__)
 # Create your views here.
 def index(request):
-    return render(request,'posapp/index.html')
+    try:
+        return render(request,'posapp/index.html')
+    except:
+        return HttpResponseNotFound
 
 def configure_items(request):
-    all_items = Item.objects.all()
-    context = {}
-    if(request.method=="POST" and request.is_ajax()):
-        id = request.POST.get("id")
-        item = all_items.get(id=id)
-        context['item'] = item
-    context['items'] = all_items
-    return render(request,'posapp/configure_items.html',context)
+    try:
+        all_items = Item.objects.all()
+        context = {}
+        if(request.method=="POST" and request.is_ajax()):
+            id = request.POST.get("id")
+            item = all_items.get(id=id)
+            context['item'] = item
+        context['items'] = all_items
+        return render(request,'posapp/configure_items.html',context)
+    except:
+        return HttpResponseBadRequest
 
 def add_item(request):
     if(request.method == "POST"):
-       item_name = request.POST.get("item_name")
-       item_price = request.POST.get("item_price")
-       Item.objects.create(item_name=item_name, item_price=item_price)
-    return redirect('configure_items')
+        try:
+            item_name = request.POST.get("item_name")
+            item_price = request.POST.get("item_price")
+            Item.objects.create(item_name=item_name, item_price=item_price)
+            return redirect('configure_items')
+        except:
+            return HttpResponseBadRequest
+    else:
+        return HttpResponseNotAllowed
 
 def delete_item(request, pk):
-    Item.objects.filter(pk=pk).delete()
-    return redirect('configure_items')
+    try:
+        Item.objects.filter(pk=pk).delete()
+        return redirect('configure_items')
+    except:
+        return HttpResponseBadRequest
 
 def update_item(request,pk):
     if(request.method=="POST"):
-        item_name = request.POST.get("item_name")
-        item_price = request.POST.get("item_price")
-        stock_quantity = request.POST.get("stock_quantity")
-        to_update = Item.objects.get(pk=pk)
-        to_update.item_name = item_name
-        to_update.item_price = item_price
-        to_update.stock_quantity = stock_quantity
-        to_update.save()
-    return redirect('configure_items')
+        try:
+            item_name = request.POST.get("item_name")
+            item_price = request.POST.get("item_price")
+            stock_quantity = request.POST.get("stock_quantity")
+            to_update = Item.objects.get(pk=pk)
+            to_update.item_name = item_name
+            to_update.item_price = item_price
+            to_update.stock_quantity = stock_quantity
+            to_update.save()
+            return redirect('configure_items')
+        except:
+            return HttpResponseBadRequest
+    else:
+        return HttpResponseNotAllowed
 
 def create_order(request):
     # Create new order with order items passed in the request object.
-    new_order = Order.objects.create()
-    new_order.save()
-    return redirect('order_details',pk=new_order.id)
+    try:
+        new_order = Order.objects.create()
+        new_order.save()
+        return redirect('order_details',pk=new_order.id)
+    except:
+        return HttpResponseBadRequest
 
 def order_details(request, pk):
     # Update order data with particular pk based on request object data.
-    order = Order.objects.get(pk=pk)
-    running_total = 0
-    for order_item in order.orderitem_set.all():
-        running_total += order_item.line_total
-    order.total_amount_paid = running_total
-    order.save()
-    all_items = Item.objects.all()
-    return render(request,'posapp/order_details.html',{
-        'order': order,
-        'items': all_items,
-    })
+    try:
+        order = Order.objects.get(pk=pk)
+        running_total = 0
+        for order_item in order.orderitem_set.all():
+            running_total += order_item.line_total
+        order.total_amount_paid = running_total
+        order.save()
+        all_items = Item.objects.all()
+        return render(request,'posapp/order_details.html',{
+            'order': order,
+            'items': all_items,
+        })
+    except:
+        return HttpResponseNotFound
 
 def list_orders(request):
     # List down all orders
-    all_orders = Order.objects.all()
-    return render(request,'posapp/list_orders.html', { 'orders' : all_orders })
+    try:
+        all_orders = Order.objects.all()
+        return render(request,'posapp/list_orders.html', { 'orders' : all_orders })
+    except:
+        return HttpResponseNotFound
 
 def update_order(request, pk):
     order = Order.objects.get(pk=pk)
     if(request.method=="POST"):
-        running_total = 0
-        for item_data in request.POST.getlist('items'):
-            order_item = OrderItem.objects.get_or_create(pk=item_data.pk)
-            order_item.item_type = Item.objects.get(id=item_data.item_id)
-            order_item.order = order
-            order_item.quantity = int(item_data.quantity)
-            order_item.line_amount = float(order_item.quantity) * float(order_item.item_type.item_price)
-            order_item.save()
-            running_total += order_item.line_amount
-        order.total_amount_paid = running_total
-        order.payment_type = request.POST.get('payment_type')
-        order.save()
-    return redirect('order_details',pk=order.id)
+        try:
+            running_total = 0
+            for item_data in request.POST.getlist('items'):
+                order_item = OrderItem.objects.get_or_create(pk=item_data.pk)
+                order_item.item_type = Item.objects.get(id=item_data.item_id)
+                order_item.order = order
+                order_item.quantity = int(item_data.quantity)
+                order_item.line_amount = float(order_item.quantity) * float(order_item.item_type.item_price)
+                order_item.save()
+                running_total += order_item.line_amount    
+            order.total_amount_paid = running_total
+            order.payment_type = request.POST.get('payment_type')
+            order.save()
+            return redirect('order_details',pk=order.id)
+        except:
+            return HttpResponseServerError
+    else:
+        return HttpResponseNotAllowed
 
 
 def add_order_item(request,pk):
@@ -96,10 +129,15 @@ def add_order_item(request,pk):
                                                         line_total=order_item_quantity*item.item_price)
             item.save()
             order.save()
+            return redirect('order_details',pk=order.id)
         else:
             return HttpResponseBadRequest()
-    return redirect('order_details',pk=order.id)
+    else:
+        return HttpResponseNotAllowed
 
 def delete_order(request,pk):
-    Order.objects.filter(pk=pk).delete()
-    return redirect('list_orders')
+    try:
+        Order.objects.filter(pk=pk).delete()
+        return redirect('list_orders')
+    except:
+        return HttpResponseBadRequest
